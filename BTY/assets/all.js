3663629a9d3cd51f7f85bae0efcc535267f54ce1
swalke16 <swalke16@gmail.com>
2013-06-28 22:36:40 -0400

    updated retina image names and stylesheets to support shopify asset urls

--- BTY/assets/all.js
+++ BTY/assets/all.js
@@ -1,9 +1,17 @@
 
 $(document).ready(function(){
-  $('nav a').click(function(e){
-    // e.preventDefault();
+  $('nav a.affiliates_link').click(function(e){
+    e.preventDefault();
     $('.dropdown').toggleClass('dropdown_open');
     $(this).toggleClass('selected');
-    $(this).siblings().removeClass('selected');
+  })
+
+  $('.color').click(function(e){
+    $(this).siblings().addClass('fade');
+    $(this).removeClass('fade');
+  })
+  $('.sizes').click(function(e){
+    $(this).siblings().addClass('fade');
+    $(this).removeClass('fade');
   })
 });
