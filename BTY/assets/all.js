2d9bcd9d76871e97feed1e183499dd650676229a
swalke16 <swalke16@gmail.com>
2013-07-05 12:59:03 -0400

    more cart updates and login page

--- BTY/assets/all.js
+++ BTY/assets/all.js
@@ -1,17 +1,48 @@
+(function() {
+
+  $(function() {
+    $('.sign_in').click(function(e) {
+      e.preventDefault();
+      $('.modal').css('display', 'block');
+      return setTimeout(function() {
+        return $('.modal').addClass('modal_open');
+      }, 1);
+    });
+    $('.cancel_button').click(function() {
+      $('.modal').removeClass('modal_open');
+      return setTimeout(function() {
+        return $('.modal').css('display', 'none');
+      }, 300);
+    });
+    $('nav a.affiliates_link').click(function(e) {
+      e.preventDefault();
+      $('.dropdown').toggleClass('dropdown_open');
+      return $(this).toggleClass('selected');
+    });
+    $('.color').click(function() {
+      $(this).siblings().addClass('fade');
+      return $(this).removeClass('fade');
+    });
+    $('.sizes').click(function() {
+      $(this).siblings().addClass('fade');
+      return $(this).removeClass('fade');
+    });
+    if ($('.total').text() === "0") {
+      $(".checkout").css('display', 'none');
+    }
+    return $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+        $('.modal').removeClass('modal_open');
+        return setTimeout(function() {
+          return $('.modal').css('display', 'none');
+        }, 300);
+      }
+    });
+  });
+
+}).call(this);
 
 $(document).ready(function(){
-  $('nav a.affiliates_link').click(function(e){
-    e.preventDefault();
-    $('.dropdown').toggleClass('dropdown_open');
-    $(this).toggleClass('selected');
-  })
 
-  $('.color').click(function(e){
-    $(this).siblings().addClass('fade');
-    $(this).removeClass('fade');
-  })
-  $('.sizes').click(function(e){
-    $(this).siblings().addClass('fade');
-    $(this).removeClass('fade');
-  })
 });
+
